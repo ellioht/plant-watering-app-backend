e1d485aced72d5ad8408403322b4eb908667d9bd
Elliot <130664947+ellioht@users.noreply.github.com>
2023-06-27 11:37:10 +0100

    port

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 const connectToDatabase = require("./Mongo.cjs");
 const createError = require("http-errors");
 
